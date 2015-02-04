22c69630cabe4ac48f6636e5e11a869b18ce31ff
adymimos <adline.dsilva@mimos.my>
2015-02-04 18:07:26 +0800

    Update server.R

--- inst/examples/shiny/server.R
+++ inst/examples/shiny/server.R
@@ -1,29 +1,12 @@
-library(bubbles)
-library(dplyr)
-library(shinySignals)
+library(solrCloud)
 
 function(input, output, session) {
   
   # Connect to data source
-  pkgData <- connectToLogs(session)
-
-  output$bubbles <- renderBubbles({
-    if (is.null(pkgData()))
-      return(NULL)
-    
-    df <- pkgData() %>%
-      select_(label = input$by) %>%
-      group_by(label) %>%
-      tally() %>%
-      arrange(match(label, pkgData()[[input$by]]))
-    
-    value <- switch(input$scale,
-      area = sqrt(df$n),
-      radius = df$n
-    )
-    
-    bubbles(value = value, label = df$label,
-      tooltip = sprintf("%s: %d", df$label, df$n)
-    )
+  
+  output$solrCloud <- rendersolrCloud({
+   value <- 3
+   label <- 'test'
+   solrCloud(value = value, label = label)
   })
 }
