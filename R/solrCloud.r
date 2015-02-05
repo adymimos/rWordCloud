bbb7815b7213707e37f7aef56903c3c9d1b54cfe
adymimos <adline.dsilva@mimos.my>
2015-02-05 13:26:56 +0800

    Updated solrCloud.r

--- R/solrCloud.r
+++ R/solrCloud.r
@@ -1,29 +1,3 @@
-#' Bubble Chart
-#' 
-#' Creates a bubble chart.
-#' 
-#' @param value Numeric vector of values, used for sizing bubbles. The value 
-#'   will be proportional to the radius, not the area; for area, call 
-#'   \code{\link{sqrt}} on the value.
-#' @param label Character vector of textual labels to use on bubbles
-#' @param tooltip Character vector of tooltip values, to be shown on hover
-#' @param color Character vector of bubble color values, in \code{"#RRGGBB"}
-#'   format; can be length 1 or length of \code{value}
-#' @param textColor Character vector of text color values, in \code{"#RRGGBB"}
-#'   format; can be length 1 or length of \code{value}
-#' @param width The width of the widget, or \code{NULL} for default.
-#' @param height The height of the widget, or \code{NULL} for default.
-#'   
-#' @return A widget object that can be printed at the console, outputted as part
-#'   of an R Markdown document, or used in a Shiny app with 
-#'   \code{\link{renderBubbles}}.
-#'   
-#' @examples
-#' bubbles(runif(10), LETTERS[1:10], color = rainbow(10, alpha = NULL))
-#'   
-#' @import htmlwidgets
-#'   
-#' @export
 solrCloud <- function(content, label, tooltip = "", color = "#EEEEEE",
   textColor = "#333333", width = NULL, height = NULL) {
 
@@ -50,23 +24,14 @@ solrCloud <- function(content, label, tooltip = "", color = "#EEEEEE",
   )
 }
 
-#' Bubble chart functions for Shiny
-#' 
-#' @param outputId Shiny output ID for this bubble chart
-#' @param width,height Plot width/height. Must be a valid CSS unit (e.g. "100%",
-#'   "400px") or a number, which will be interpreted as pixels ("px").
-#' @param expr An expression that generates a \code{\link{bubbles}} widget.
-#' @param env The environment in which to evaluate \code{expr}.
-#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})?
-#'   
-#' @export
+
 solrCloudOutput <- function(outputId, width = '600px', height = '600px'){
   shinyWidgetOutput(outputId, 'solrCloud', width, height, package = 'solrCloud')
 }
 
 #' @rdname bubblesOutput
 #' @export
-renderBubbles <- function(expr, env = parent.frame(), quoted = FALSE) {
+rendersolrCloud <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
   shinyRenderWidget(expr, solrCloudOutput, env, quoted = TRUE)
 }
